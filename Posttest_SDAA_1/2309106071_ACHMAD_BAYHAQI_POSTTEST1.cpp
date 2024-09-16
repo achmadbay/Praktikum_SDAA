0441433d78bd212597be571c19b0b17df9810271
achmadbay <158174114+achmadbay@users.noreply.github.com>
2024-09-16 14:40:16 +0800

    Posttest_SDAA_1

---
+++ Posttest_SDAA_1/2309106071_ACHMAD_BAYHAQI_POSTTEST1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+void hanoi(int n, char asal, char tujuan, char sementara) {
+    if (n == 1) {
+        cout << "Pindahkan piringan 1 dari " << asal << " ke " << tujuan << endl;
+        return;
+    }
+    
+    hanoi(n - 1, asal, sementara, tujuan);
+    cout << "Pindahkan piringan " << n << " dari " << asal << " ke " << tujuan << endl;
+    hanoi(n - 1, sementara, tujuan, asal);
+}
+
+int main() {
+    int n;
+    cout << "Masukkan jumlah piringan: ";
+    cin >> n;
+
+    cout << "Langkah-langkah untuk menyelesaikan Menara Hanoi dengan " << n << " piringan:" << endl;
+    hanoi(n, 'A', 'C', 'B'); // A adalah menara asal, C adalah menara tujuan, B adalah menara sementara
+
+    return 0;
+}
